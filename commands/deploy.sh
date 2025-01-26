92a97a72b972d6db8b9a096b0a37c126953d5bee
Yurii Okal <yurii.okal@tangramltd.com>
2025-01-26 19:07:35 +0200

    - change command syntax dor docker v2 in deploy.sh

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -24,8 +24,8 @@ git reset --hard origin/main || handle_error "Failed to reset the local reposito
 echo "Fetching tags from the remote repository..."
 git fetch origin --tags || handle_error "Failed to fetch tags from the 'origin' remote."
 
-# Build and run Docker containers with Docker Compose
-docker-compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
+# Build and run Docker containers with Docker Compose v2
+docker compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
 
 # Print a success message upon successful deployment
 echo "Deployment completed successfully."
