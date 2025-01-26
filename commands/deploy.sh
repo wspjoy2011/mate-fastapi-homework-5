5f81e1fe03696f962f97daa4a08bd2f5721600cc
Yurii Okal <yurii.okal@tangramltd.com>
2025-01-26 18:04:16 +0200

    - fix git pull command in deploy.sh

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -13,7 +13,7 @@ handle_error() {
 cd /home/ubuntu/src/mate-fastapi-homework-5 || handle_error "Failed to navigate to the application directory."
 
 # Update the code from the repository
-git pull mate main || handle_error "Failed to pull the latest code from the 'mate' repository on the 'main' branch."
+git pull || handle_error "Failed to pull the latest code from the 'mate' repository on the 'main' branch."
 
 # Build and run Docker containers with Docker Compose
 docker-compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
