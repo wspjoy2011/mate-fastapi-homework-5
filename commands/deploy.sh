9e4de167b88cba9f0ed22b6fb82e1a705d31a0ad
Yurii Okal <yurii.okal@tangramltd.com>
2025-01-26 17:56:19 +0200

    - add deploy to aws ec2

---
+++ commands/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit the script immediately if any command exits with a non-zero status
+set -e
+
+# Function to handle errors with custom messages
+handle_error() {
+    echo "Error: $1"
+    exit 1
+}
+
+# Navigate to the application directory
+cd /home/ubuntu/src/mate-fastapi-homework-5 || handle_error "Failed to navigate to the application directory."
+
+# Update the code from the repository
+git pull mate main || handle_error "Failed to pull the latest code from the 'mate' repository on the 'main' branch."
+
+# Build and run Docker containers with Docker Compose
+docker-compose -f docker-compose-prod.yml up -d --build || handle_error "Failed to build and run Docker containers using docker-compose-prod.yml."
+
+# Print a success message upon successful deployment
+echo "Deployment completed successfully."
